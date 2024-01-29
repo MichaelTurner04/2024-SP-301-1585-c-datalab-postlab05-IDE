413dd3362a9739478a367cf85c67ac3f631c63cd
stcscpp <stcscpp@umsystem.edu>
2024-01-28 23:19:08 -0600

    init

---
+++ funcs.cpp
@@ -0,0 +1,14 @@
+unsigned long factorial(unsigned long n)
+{
+    if(n < 2)
+    {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+unsigned long combination(unsigned long n, unsigned long m)
+{
+    //TODO: Implement me!
+}
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "funcs.h"
+#include "TrianglePrinter.h"
+
+using namespace std;
+
+int main()
+{
+    TrianglePrinter t;
+
+    for(unsigned int i = 0; i < 10; i++)
+    {
+        cout << factorial(i) << endl;
+    }
+
+    return 0;
+}
