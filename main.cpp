cd805948cd89fe789802b9ff28a3be35e5e5e444
mturner <mjt6yf@umsystem.edu>
2024-03-05 17:22:57 -0600

    problem 2 completed

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 #include "TrianglePrinter.h"
 
 using namespace std;
-
 int main()
 {
     TrianglePrinter t;
-
+    for(int r=0; r<7; r++){
+        for(int c=0; c<= r; c++){
+            t.add(combination(r, c));
+        }t.newrow();
+    }
+    t.print();
     for(unsigned int i = 0; i < 10; i++)
     {
         cout << factorial(i) << endl;
@@ -19,12 +23,5 @@ int main()
     cout << "Enter value for M."<< endl;
     cin >> m;
     cout << combination(n,m)<< endl;
-    /*
-    for (long n = 0; n<= 5; n++){
-        for(long m = 0; m<=n; m++){
-            cout << "("<<n <<", " << m << ") = " << combination(n, m) << endl;
-        }
-    }
-    */
     return 0;
 }
